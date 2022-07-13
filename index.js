a31a3388bbafb4063dd698e03873414571ba8d21
nerincon21 <58572706+nerincon21@users.noreply.github.com>
2022-07-13 17:42:26 -0600

    test

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use('/pre', preSolicitudes);
 
 // Definir un puerto y arrancar el proyecto
 const port = 3000;
-const host = 'https://apicrudtest2.herokuapp.com'
-app.listen(port, host,() => {
-    console.log(`El Servidor${host}está funcionando en el puerto ${port}`)
+
+app.listen(port,() => {
+    console.log(`El Servidor está funcionando en el puerto ${port}`)
 });
