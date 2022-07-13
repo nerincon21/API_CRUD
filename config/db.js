9361e02ccd09ce428bb3fdf871211cf1a0cf9e14
nerincon21 <58572706+nerincon21@users.noreply.github.com>
2022-07-13 15:34:48 -0600

    test

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('crudapi', 'root', '', {
+const db = new Sequelize('localhost', 'root', '', {
   host: '127.0.0.1',
   dialect: 'mysql',
   port: '3306',
