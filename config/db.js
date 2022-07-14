d59d36f57fe2f4657f0ba576d27b250af41bd9ab
nerincon21 <58572706+nerincon21@users.noreply.github.com>
2022-07-13 23:16:45 -0600

    test

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('heroku_5a8cd2b924a4c18', 'ba135fc9aceecf', '1d29e835', {
-  host: 'us-cdbr-east-06.cleardb.net',
+const db = new Sequelize('sql3506337', 'sql3506337', 'RDZXeDH1AE', {
+  host: 'sql3.freesqldatabase.com',
   dialect: 'mysql',
   port: '3306',
   operatorsAliases: 0,
