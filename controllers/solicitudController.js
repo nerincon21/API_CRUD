26a37b6e2a259cb0ab2e05180f33758d57fa82b0
nerincon21 <58572706+nerincon21@users.noreply.github.com>
2022-07-19 11:53:42 -0600

    Editando cositas

--- controllers/solicitudController.js
+++ controllers/solicitudController.js
@@ -43,38 +43,38 @@ exports.crearSolicitud = async (req, res) => {
     console.log('<*---Crear---*>');
     console.log(req.body); //Bandera para los datos
 
-    if (!req.body.cliente==undefined) { //Si no se leen datos 
+    if (!(JSON.stringify(req.body)=='{}')) { //Si no se leen datos 
 
-    console.log("<----Leí los datos--->");
-    console.log(req.body.cliente);
-    var cliente = req.body.cliente.tipo;
-    cliente = cliente.substring(0, 1); //Acortamiento del tipo de cliente a 1 solo caracter Fisico (f) / Moral (m)
-    
-    console.log(cliente); //Se corrobora el acortamiento de cliente tipo
+      console.log("<----Leí los datos--->");
+      console.log(req.body.cliente);
+      var cliente = req.body.cliente.tipo;
+      cliente = cliente.substring(0, 1); //Acortamiento del tipo de cliente a 1 solo caracter Fisico (f) / Moral (m)
+      
+      console.log("Cliente tipo ", cliente); //Se corrobora el acortamiento de cliente tipo
 
 
-    //Crear la Solicitud
-    Solicitudes.create({
-        cliente, 
-        nombre : req.body.cliente.fisica.nombre, 
-        paterno : req.body.cliente.fisica.paterno, 
-        materno : req.body.cliente.fisica.materno, 
-        razon_social : req.body.cliente.moral.razon_social, 
-        representante : req.body.cliente.moral.representante, 
-        rfc : req.body.cliente.rfc, 
-        curp : req.body.cliente.fisica.curp, 
-        email: req.body.cliente.email, 
-        celular : req.body.cliente.celular, 
-        calle : req.body.domicilio.calle, 
-        interior : req.body.domicilio.interior, 
-        exterior : req.body.domicilio.exterior, 
-        colonia : req.body.domicilio.colonia, 
-        c_postal : req.body.domicilio.c_postal, 
-        poblacion : req.body.domicilio.poblacion, 
-        estado : req.body.domicilio.estado, 
-        municipio : req.body.domicilio.municipio,
-        /*ine_fisico, buro_fisico, comprobante_domicilio, situacion_fiscal, ine_representante, buro_moral*/
-    })        
+      //Crear la Solicitud
+      Solicitudes.create({
+          cliente, 
+          nombre : req.body.cliente.fisica.nombre, 
+          paterno : req.body.cliente.fisica.paterno, 
+          materno : req.body.cliente.fisica.materno, 
+          razon_social : req.body.cliente.moral.razon_social, 
+          representante : req.body.cliente.moral.representante, 
+          rfc : req.body.cliente.rfc, 
+          curp : req.body.cliente.fisica.curp, 
+          email: req.body.cliente.email, 
+          celular : req.body.cliente.celular, 
+          calle : req.body.domicilio.calle, 
+          interior : req.body.domicilio.interior, 
+          exterior : req.body.domicilio.exterior, 
+          colonia : req.body.domicilio.colonia, 
+          c_postal : req.body.domicilio.c_postal, 
+          poblacion : req.body.domicilio.poblacion, 
+          estado : req.body.domicilio.estado, 
+          municipio : req.body.domicilio.municipio,
+          /*ine_fisico, buro_fisico, comprobante_domicilio, situacion_fiscal, ine_representante, buro_moral*/
+      })        
 
 
     } else {
@@ -101,9 +101,8 @@ exports.solicitudesShow = async (req, res) => {
 exports.editar_solicitudes = async (req, res) => {
   console.log('<*---Editar---*>');
     console.log(req.params);
-    // const solicitudes_edit =  await Solicitudes.findByPk();
     const solicitudes_edit =  await Solicitudes.findByPk(req.params.id);
-   
+    res.json(solicitudes_edit)
     
     console.log(solicitudes_edit);
 }
