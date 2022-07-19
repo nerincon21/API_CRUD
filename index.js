2a6b4eba9d47d164927c081c2e61b9e25757db4b
nerincon21 <58572706+nerincon21@users.noreply.github.com>
2022-07-19 15:52:39 -0600

    Cabeceras y cors

--- index.js
+++ index.js
@@ -38,6 +38,16 @@ app.use(bodyParser.json({ type: 'application/json' }));
 
 app.use(cors());
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 
 //A ver que pedo
 //API REST
