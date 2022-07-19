418b0b0ba39c161d68e68bb99df3c511431ba737
nerincon21 <58572706+nerincon21@users.noreply.github.com>
2022-07-19 11:05:06 -0600

    Ajustes menores

--- controllers/solicitudController.js
+++ controllers/solicitudController.js
@@ -1,7 +1,7 @@
 const Solicitudes = require('../models/Solicitudes');
 
 exports.test = (req, res) =>{
-  const msg = "<--------------Hola Consola-------------->"
+  const msg = "<--------------Hola Consola soy el Test-------------->"
   console.log(msg);
   res.json({
     "cliente": {
@@ -40,94 +40,22 @@ exports.test = (req, res) =>{
 }
 
 exports.crearSolicitud = async (req, res) => {
-    console.log('Hola Crear');
-    console.log(req.body);
+    console.log('<*---Crear---*>');
+    console.log(req.body); //Bandera para los datos
 
+    if (!req.body.cliente==undefined) { //Si no se leen datos 
 
-    
-    // leer los datos
-     console.log(req.body.cliente);
+    console.log("<----Leí los datos--->");
+    console.log(req.body.cliente);
     var cliente = req.body.cliente.tipo;
-    cliente = cliente.substring(0, 1);
+    cliente = cliente.substring(0, 1); //Acortamiento del tipo de cliente a 1 solo caracter Fisico (f) / Moral (m)
     
-    
-    console.log("Leí los datos");
-    //console.log(req.body.cliente.fisica.curp);
-    console.log(cliente);
-
-        Solicitudes.create({
-            cliente, 
-          
-            nombre : req.body.cliente.fisica.nombre, 
-            paterno : req.body.cliente.fisica.paterno, 
-            materno : req.body.cliente.fisica.materno, 
-            razon_social : req.body.cliente.moral.razon_social, 
-            representante : req.body.cliente.moral.representante, 
-            rfc : req.body.cliente.rfc, 
-            curp : req.body.cliente.fisica.curp, 
-            email: req.body.cliente.email, 
-            celular : req.body.cliente.celular, 
-            calle : req.body.domicilio.calle, 
-            interior : req.body.domicilio.interior, 
-            exterior : req.body.domicilio.exterior, 
-            colonia : req.body.domicilio.colonia, 
-            c_postal : req.body.domicilio.c_postal, 
-            poblacion : req.body.domicilio.poblacion, 
-            estado : req.body.domicilio.estado, 
-            municipio : req.body.domicilio.municipio,
-            /*ine_fisico, buro_fisico, comprobante_domicilio, situacion_fiscal, ine_representante, buro_moral*/
-        })        
-        // redirigir al usuario
-        //req.flash('correcto', 'Presolicitud Hecha');
-        
-}
-
-exports.solicitudesShow = async (req, res) => {
-  console.log('Hola show');
-    //console.log(req.body);
-    console.log("Estás en solicitudes show");
-    const solicitudesPromise = Solicitudes.findAll({
-        attributes: ['id', 'cliente','nombre','paterno','materno','rfc','curp','email','celular']   
-      });
-
-    const [solicitudes] = await Promise.all([solicitudesPromise]);
-//    console.log(solicitudes);
-    //console.log(solicitudes)
-    // render a la vista   
-    res.json(solicitudes)
-}
-
-// Me mande el ID 
-exports.editar_solicitudes = async (req, res) => {
-  console.log('Hola edit');
-    console.log(req.params);
-    // const solicitudes_edit =  await Solicitudes.findByPk();
-    const solicitudes_edit =  await Solicitudes.findByPk(req.params.id);
-   
-    
-    console.log(solicitudes_edit);
-}
-
-exports.updateSolicitud = async (req, res) => {
-  console.log('Hola update');
-    const ricuest = req.body;
-    console.log('--------------------------Riquest----------------------');
-    console.log(ricuest);
-    
-    var cliente = req.body.cliente.tipo;
-    cliente = cliente.substring(0, 1);
-
-/*     await User.update({ lastName: "Doe" }, {
-        where: {
-          lastName: null
-        }
-      }); */
+    console.log(cliente); //Se corrobora el acortamiento de cliente tipo
 
 
-    await Solicitudes.update({ 
-
+    //Crear la Solicitud
+    Solicitudes.create({
         cliente, 
-        
         nombre : req.body.cliente.fisica.nombre, 
         paterno : req.body.cliente.fisica.paterno, 
         materno : req.body.cliente.fisica.materno, 
@@ -146,19 +74,80 @@ exports.updateSolicitud = async (req, res) => {
         estado : req.body.domicilio.estado, 
         municipio : req.body.domicilio.municipio,
         /*ine_fisico, buro_fisico, comprobante_domicilio, situacion_fiscal, ine_representante, buro_moral*/
+    })        
 
-        }, {
-        where: {
-          id: req.body.cliente.id
-        }
-      });
+
+    } else {
+      console.log("No estoy recibiendo datos");
+      return;
+    }
+
+
+}
+
+exports.solicitudesShow = async (req, res) => {
+  console.log(' <*---Show---*> ');
+  console.log("Estás en solicitudes show");
+  
+  const solicitudesPromise = Solicitudes.findAll({
+      attributes: ['id', 'cliente','nombre','paterno','materno','rfc','curp','email','celular']   
+    });
+
+  const [solicitudes] = await Promise.all([solicitudesPromise]);
+  res.json(solicitudes)
+}
+
+// Me mande el ID 
+exports.editar_solicitudes = async (req, res) => {
+  console.log('<*---Editar---*>');
+    console.log(req.params);
+    // const solicitudes_edit =  await Solicitudes.findByPk();
+    const solicitudes_edit =  await Solicitudes.findByPk(req.params.id);
+   
+    
+    console.log(solicitudes_edit);
+}
+
+exports.updateSolicitud = async (req, res) => {
+  console.log('<*---Actualizar---*>');
+  console.log(req.body); //Bandera para los datos
+    
+  var cliente = req.body.cliente.tipo;
+  cliente = cliente.substring(0, 1); //Acortamiento del tipo de cliente a 1 solo caracter Fisico (f) / Moral (m)
+  console.log(cliente); //Se corrobora el acortamiento de cliente tipo
+
+  await Solicitudes.update({ 
+    cliente, 
+    nombre : req.body.cliente.fisica.nombre, 
+    paterno : req.body.cliente.fisica.paterno, 
+    materno : req.body.cliente.fisica.materno, 
+    razon_social : req.body.cliente.moral.razon_social, 
+    representante : req.body.cliente.moral.representante, 
+    rfc : req.body.cliente.rfc, 
+    curp : req.body.cliente.fisica.curp, 
+    email: req.body.cliente.email, 
+    celular : req.body.cliente.celular, 
+    calle : req.body.domicilio.calle, 
+    interior : req.body.domicilio.interior, 
+    exterior : req.body.domicilio.exterior, 
+    colonia : req.body.domicilio.colonia, 
+    c_postal : req.body.domicilio.c_postal, 
+    poblacion : req.body.domicilio.poblacion, 
+    estado : req.body.domicilio.estado, 
+    municipio : req.body.domicilio.municipio,
+    /*ine_fisico, buro_fisico, comprobante_domicilio, situacion_fiscal, ine_representante, buro_moral*/
+
+    }, {
+    where: {
+      id: req.body.cliente.id
+    }
+  });
 
 }
 
 exports.delete = async (req, res) => {
-    const respuntoparams = res.params;
-    console.log(respuntoparams);
-
+  console.log('<*---Borrar---*>');
+    console.log(res.params);
     await Solicitudes.destroy({
         where: {
           id: req.params.id
