1e769ba73fcbc375283cb6b0ca26314d4e7ff1e9
nerincon21 <58572706+nerincon21@users.noreply.github.com>
2022-07-21 12:56:30 -0600

    Cambio de DB freesql

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const mysql2 = require('mysql2');
 
 const db = new Sequelize('sql3507916', 'sql3507916', 'CrT9wuNnc2', {
-  host: 'sql3.freesqldatabase.com	',
+  host: 'sql3.freesqldatabase.com',
   dialect: 'mysql',
   dialectModule: require('mysql2'),
   port: '3306',
