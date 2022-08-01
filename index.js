68bb146ac323462618ff291a6eab60e696e984fb
nerincon21 <58572706+nerincon21@users.noreply.github.com>
2022-08-01 13:25:51 -0600

    pedos

--- index.js
+++ index.js
@@ -50,13 +50,7 @@ app.use(multer({
         }   
         cb("Error: La extensión del archivo no es válida");
     }
-}).fields([
-    {name: "ine_fisico", maxCount: 1},
-    {name: "buro_fisico", maxCount: 1},
-    {name: "comprobante_domicilio", maxCount: 1},
-    {name: "situacion_fiscal", maxCount: 1},
-    {name: "ine_reprecentante", maxCount: 1},
-    {name: "buro_moral", maxCount: 1}])
+}).fields([])
     );
 
 // cargar los archivos estaticos
